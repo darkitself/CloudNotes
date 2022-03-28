5f1729d4258c91ec5277dc5e76e74c0b7ae411d4
AlexScheglov <my-income@bk.ru>
2022-03-28 15:58:12 +0500

    2. Implement database interaction using Hibernate Create entities

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+}
